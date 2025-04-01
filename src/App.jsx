adc175ba248df20888a62c7db52e56ebd1d691d3
Gustavo Diques <diquesg@gmail.com>
2025-04-01 15:41:28 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { nanoid } from "nanoid"
 import Die from './components/Die'
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 
 function App() {
+  const { width, height } = useWindowSize()
 
   function generateRandomValue() {
     return Math.ceil(Math.random() * 6)
@@ -16,7 +19,7 @@ function App() {
     return diceObjArray
   }
 
-  const [dice, setDice] = useState(generateAllNewDice)
+  const [dice, setDice] = useState(() => generateAllNewDice())
 
   const diceElements = dice.map((dieObj) => <Die value={dieObj.value} key={dieObj.id} isHeld={dieObj.isHeld} hold={() => hold(dieObj.id)} />)
 
@@ -31,8 +34,20 @@ function App() {
     ))
   }
 
+  const gameWon = dice.every(dieObj => dieObj.value === dice[0].value) && dice.every(dieObj => dieObj.isHeld === true)
+
+  const gameWonRef = useRef(null)
+
+  useEffect(() => {
+    gameWon && gameWonRef.current.focus()
+  }, [gameWon])
+
   return (
     < main >
+      {gameWon && <Confetti width={width} height={height} />}
+      <div aria-live='polite' className='sr-only'>
+        {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+      </div>
       <div className='dice-content'>
         <div className="description">
           <h1>Tenzies</h1>
@@ -41,7 +56,7 @@ function App() {
         <div className="dice-wrapper">
           {diceElements}
         </div>
-        <button onClick={rollDice} className="roll-button">Roll</button>
+        <button onClick={gameWon ? () => setDice(generateAllNewDice) : rollDice} className="roll-button" ref={gameWonRef}>{gameWon ? "New Game" : "Roll"}</button>
       </div>
     </main>
   )
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,5 +1,12 @@
 export default function Die(props) {
     return (
-        <button className={props.isHeld ? "die-button-isheld" : "die-button"} onClick={props.hold}>{props.value}</button>
+        <button
+            className={props.isHeld ? "die-button-isheld" : "die-button"}
+            onClick={props.hold}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, ${props.isHeld ? "held" : "not held"}`}
+        >
+            {props.value}
+        </button>
     )
 }
